a12c383528d07b8a1a1fb88b73d91c993681320e
DALeibovich <105238549+DALeibovich@users.noreply.github.com>
2022-06-15 09:31:38 -0300

    Entregable express

---
+++ entregable_express/Clases/Contenedor.js
@@ -0,0 +1,85 @@
+
+module.exports = class Contenedor {
+    path_db = './DB/';
+    arrObjetos = [];
+
+    constructor(nombrearchivo, fs = 0) {
+        this.nombrearchivo = nombrearchivo;
+        this.fs = fs;
+
+    }
+
+    async save(objeto) {
+        try {
+            let arr = await this.getAll() ?? [];
+            this.arrObjetos = [...arr];
+            let id = 1;
+          
+            if (arr.length > 0) {
+                id = arr[arr.length - 1].id + 1;
+            }
+            let objetoNuevo = Object.assign({ id: id }, objeto);
+            this.arrObjetos.push(objetoNuevo);
+            await this.fs.promises.writeFile(this.path_db + this.nombrearchivo, JSON.stringify(this.arrObjetos, null, 2));
+            // console.log('Id: ' + id);
+            return id;
+        } catch (err) {
+            console.log("Se produjo un error: " + err);
+            return null;
+        }
+    }
+
+    async getById(id) {
+        let arr = await this.getAll() ?? [];
+        this.arrObjetos = [...arr];
+        let objeto = arr.find((obj) => obj.id === id) ?? null;
+        return objeto;
+    }
+
+    async getAll() {
+        try {
+            let arr = await this.fs.promises.readFile(this.path_db + this.nombrearchivo, 'utf-8');           
+            this.arrObjetos = JSON.parse(arr);
+            //console.log(this.arrObjetos)
+            return this.arrObjetos;
+            
+        } catch (err) {
+            if (err.code === 'ENOENT') console.log("Archivo no existe -> Creado con exito");
+            return null;
+        }
+    }
+
+    async deleteAll() {
+        try {
+            let arr = await this.getAll() ?? [];
+            if (arr.length > 0) {
+                this.arrObjetos = [];
+                this.fs.promises.writeFile(this.path_db + this.nombrearchivo, "[]");
+                return 'Todos los registros fueron borrado con exito';
+            } else {
+                return 'No existe objetos para borrar';
+            }
+        } catch (err) {
+            return null;
+        }
+
+    }
+
+    async deleteById(id) {
+        let objeto = await this.getById(id);
+        if (objeto !== null) {
+            try {
+                let arrResultado = this.arrObjetos.filter(objeto => objeto.id !== id);
+                await this.fs.promises.writeFile(this.path_db + this.nombrearchivo, JSON.stringify(arrResultado, null, 2));
+                return `id ${id} borrado con exito`;
+            } catch (err) {
+                return null;
+            }
+        } else {
+            // console.log('id no encontrado');
+            return `id ${id} no encontrado`;
+        }    
+    }
+
+}
+
---
+++ entregable_express/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 8080;
+
+const nombreArchivo = 'productos.txt';
+const Contenedor = require('./Clases/Contenedor.js');
+let archivoLibros = new Contenedor(nombreArchivo, fs);
+let productos = [];
+
+// obtenemos todos los productos del archivo
+archivoLibros.getAll()
+.then(res => productos = res)
+.catch(err => console.log(err));
+
+// Creacion del servidor
+const server = app.listen(port, () => {
+    console.log(`Servidor escuchado en el puerto ${server.address().port}`);
+});
+
+
+// endPoint para todos los productos
+app.get('/productos', (req, res) => {
+  res.send(`<pre>${JSON.stringify(productos,null,2)}</pre>`);
+    
+});
+
+// endPoint para productos random
+app.get('/productoRandom', (req, res) => {
+    let idRandom = parseInt(Math.random() * productos.length);
+    res.send(`<pre>${JSON.stringify(productos[idRandom],null,2)}</pre>`);
+});
