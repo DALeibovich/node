bba5904496df59ee08e53df2d2ee503caa16af46
DALeibovich <105238549+DALeibovich@users.noreply.github.com>
2022-07-14 10:15:59 -0300

    Entregable Faker - Normalizr

---
+++ entregable_mocks/README.md
@@ -0,0 +1,58 @@
+# FAKER-NORMALIZR + BASE DE DATOS + WEBSOCKETS + MOTORES DE PLANTILLAS
+#### Se usa persistencia en Archivos (/DB/foro-normalizr.json)
+## Ejecución
+1) ejecutar node ./server.js o  npm start
+
+### Modulos necesarios
+<br> normalizr, express, handlebars, pug, ejs, socket.io, knex, mysql, sqlite3
+
+### Simulador
+1) http://127.0.0.1:8080/
+
+### FAKER
+#### /controllers/faker.js
+#### /controllers/productos.js
+
+### NORMALIZR
+#### /DB/foro-normalizr.js
+#### /controllers/foros.js
+#### /public/main.js
+
+### Estructura de BASE DE DATOS
+#### /scriptTables.js
+#### /DB/ecommerce.sqlite
+#### /models/ [KnexDB.js | Contenedor.js]
+#### /controllers/conexionDB.js
+#### /controllers/config/ [mysql.js | sqlite3.js]
+
+
+
+
+### Estructura de archivos HTML. HBS, PUG, EJS
+
+<br> /public/index.html
+<br> /public/plantillas/productos.hbs
+<br> /public/plantillas/mensajes.hbs
+
+<br> /views/productos.hbs
+<br> /views/productos.pug
+<br> /views/productos.ejs
+
+<br> /views/layouts/index.hbs
+<br> /views/layouts/index.pug
+<br> /views/layouts/index.ejs
+
+### Estructura de archivos JS
+<br>/index.js
+<br>/controllers/productos.js
+<br>/routes/productos.js
+<br>/routes/foros.js
+<br>/models/Contenedor.js
+<br>/DB/productos.txt
+<br>/DB/foros.txt
+
+### endPoints
+<br>/productos/hbs
+<br>/productos/pug
+<br>/productos/ejs
+
---
+++ entregable_mocks/server.js
@@ -0,0 +1,70 @@
+/*************CONEXION A DB******************* */
+// Llama a la cracion de tablas en Mysql y SQLite3, abriendo la conexion con KnexDB
+//require('./scriptTables');
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const { emitirProductosSocket, agregarProductoSocket } = require('./controllers/productos');
+const { emitirMensajesSocket, agregarMensajeSocket } = require('./controllers/foros');
+
+
+/**********CONFIGURACION DEL ENTORNO ************************************************ */
+// para capturar tipos de datos y no solo string
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// para que tome la carpeta publica or defecto y el index.html contenido
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+/******** SERVER WEBSOCKET *************************************************/
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+// escuchamos la conexion del socket
+io.on('connection', function (socket) {
+    //emitimos los productos a los clientes
+    emitirProductosSocket(io, socket);
+    //Agregamos un producto enviado por el cliente
+    agregarProductoSocket(io, socket);
+
+    //emitimos los mensajes del foro
+    emitirMensajesSocket(io, socket);
+    //Agregamos un mensaje al foro
+    agregarMensajeSocket(io, socket);
+
+});
+
+
+/******** SERVER WEB *****************************************************************/
+const PORT = process.env.PORT || 8080;
+const serv = server.listen(PORT, () => {
+    console.log('listening on port', serv.address().port);
+})
+serv.on('error', err => console.error('listening on port', err));
+
+
+
+/******** PARTE DEL ENTREGABLE MOTORES DE PLANTILLAS *****************************************/
+// conservo del entregable anterior
+const handlebars = require('express-handlebars');
+const { engine } = handlebars;
+const productos = require('./routes/productos');
+
+app.engine(
+    "hbs",
+    engine({
+        extname: ".hbs",
+        defaultLayout: "index.hbs",
+    })
+);
+
+app.set("views", "./views"); //especifica el directorio
+app.set("view engine", "hbs");
+
+// ruteo para los productos
+app.use('/', productos);
+
+// por default si no encuetra una ruta especifica
+app.use((req, res) => {
+    res.send('endPoint no encontrado')
+});
